16569ef918a4b142355d43323c53ebed0a759c01
Teresa Atencio <teresa.atencio.robles@hotmail.com>
2021-09-21 09:46:03 -0500

    Registro exitoso, se agrego About Us

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace PROYECTO_BICICLETAS.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
         {
             return View();
         }
