c79ade5594cb110e3036937fdd8b7e264e644745
MarioVilcas <88749922+MarioVilcas@users.noreply.github.com>
2021-09-24 20:56:12 -0500

    css actualización

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace PROYECTO_BICICLETAS.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "¿Recordar contraseña?")]
             public bool RememberMe { get; set; }
         }
 
